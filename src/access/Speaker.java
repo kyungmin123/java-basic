14b8d8e747871dbf8c0140f6af5bd8b5a1cf85cc
kyungmin123 <77143146+kyungmin123@users.noreply.github.com>
2026-07-04 16:48:34 +0900

    Section 7, Access Modifier

---
+++ src/access/Speaker.java
@@ -0,0 +1,33 @@
+package access;
+
+public class Speaker {
+    private int volume;
+
+    Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if (volume >= 100) {
+            System.out.println("음량을 증가할 수 없습니다. 최대 음량입니다.");
+        } else {
+            volume += 10;
+            System.out.println("음량을 10 증가합니다.");
+        }
+    }
+
+    void volumeDown() {
+        System.out.println("volumeDown 호출");
+
+        if (volume - 10 < 0) {
+            volume = 0;
+            return;
+        }
+
+        volume -= 10;
+    }
+
+    void showVolume() {
+        System.out.println("현재 음량: " + volume);
+    }
+}
