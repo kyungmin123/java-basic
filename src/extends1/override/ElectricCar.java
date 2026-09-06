8630db22f57e055bd232e4b396f46055b0e40503
kyungmin123 <77143146+kyungmin123@users.noreply.github.com>
2026-09-06 17:16:46 +0900

    Section 10, extends

---
+++ src/extends1/override/ElectricCar.java
@@ -0,0 +1,12 @@
+package extends1.override;
+
+public class ElectricCar extends Car {
+    @Override
+    public void move() {
+
+        System.out.println("전기차를 이동합니다.");
+    }
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+}
---
+++ src/extends1/override/HydrogenCar.java
@@ -0,0 +1,7 @@
+package extends1.override;
+
+public class HydrogenCar extends Car {
+    public void fillHydrogen() {
+        System.out.println("수소를 충전합니다.");
+    }
+}
